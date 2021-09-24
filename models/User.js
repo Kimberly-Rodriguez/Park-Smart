f4c45fc00daa0fc58fff246184b6975d9a31aacf
Chris Tolnai <christolnai@gmail.com>
2021-09-24 14:56:20 -0700

    updated user.js

--- models/User.js
+++ models/User.js
@@ -19,13 +19,25 @@ User.init(
         email: {
             type: DataTypes.STRING,
             allowNull: false,
+            validate: {
+                isEmail: true,
+              },
         },
         password: {
             type: DataTypes.STRING,
             allowNull: false,
+            validate: {
+                len: [8],
+              },
         },
     },
     {
+        hooks: {
+            beforeCreate: async (newUserData) => {
+              newUserData.password = await bcrypt.hash(newUserData.password, 10);
+              return newUserData;
+            },
+          },
         sequelize,
         timestamps: true,
         freezeTableName: true,
