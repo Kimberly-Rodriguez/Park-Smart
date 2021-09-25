5bf124ef2a3d3ac24d03da565494791786aa11b2
lyndseyjw <lyndseyjwatson@gmail.com>
2021-09-24 21:01:38 -0700

    added taken & available modals to button 1

--- public/js/neighborhood.js
+++ public/js/neighborhood.js
@@ -1,42 +1,89 @@
-const newFormHandler = async (event) => {
-  event.preventDefault();
+// const newFormHandler = async (event) => {
+//   event.preventDefault();
 
-  const name = document.querySelector('#project-name').value.trim();
-  const needed_funding = document.querySelector('#project-funding').value.trim();
-  const description = document.querySelector('#project-desc').value.trim();
+//   const name = document.querySelector('#project-name').value.trim();
+//   const needed_funding = document.querySelector('#project-funding').value.trim();
+//   const description = document.querySelector('#project-desc').value.trim();
 
-  if (name && needed_funding && description) {
-    const response = await fetch(`/api/projects`, {
-      method: 'POST',
-      body: JSON.stringify({ name, needed_funding, description }),
-      headers: {
-        'Content-Type': 'application/json',
-      },
-    });
+//   if (name && needed_funding && description) {
+//     const response = await fetch(`/api/projects`, {
+//       method: 'POST',
+//       body: JSON.stringify({ name, needed_funding, description }),
+//       headers: {
+//         'Content-Type': 'application/json',
+//       },
+//     });
 
-    if (response.ok) {
-      document.location.replace('/profile');
-    } else {
-      alert('Failed to create project');
+//     if (response.ok) {
+//       document.location.replace('/profile');
+//     } else {
+//       alert('Failed to create project');
+//     }
+//   }
+// };
+
+// const delButtonHandler = async (event) => {
+//   if (event.target.hasAttribute('data-id')) {
+//     const id = event.target.getAttribute('data-id');
+
+//     const response = await fetch(`/api/projects/${id}`, {
+//       method: 'DELETE',
+//     });
+
+//     if (response.ok) {
+//       document.location.replace('/profile');
+//     } else {
+//       alert('Failed to delete project');
+//     }
+//   }
+// };
+
+var myModal = document.getElementById('myModal')
+var myInput = document.getElementById('myInput')
+
+myModal.addEventListener('shown.bs.modal', function () {
+  myInput.focus()
+})
+
+const one = document.querySelector('.one');
+const two = document.querySelector('.two');
+const three = document.querySelector('.three');
+const four = document.querySelector('.four');
+const five = document.querySelector('.five');
+const six = document.querySelector('.six');
+const seven = document.querySelector('.seven');
+const eight = document.querySelector('.eight');
+const nine = document.querySelector('.nine');
+const ten = document.querySelector('.ten');
+const eleven = document.querySelector('.eleven');
+const twelve = document.querySelector('.twelve');
+const thirteen = document.querySelector('.thirteen');
+const fourteen = document.querySelector('.fourteen');
+const fifteen = document.querySelector('.fifteen');
+const sixteen = document.querySelector('.sixteen');
+const seventeen = document.querySelector('.seventeen');
+const eighteen = document.querySelector('.eighteen');
+const nineteen = document.querySelector('.nineteen');
+const twenty = document.querySelector('.twenty');
+const twentyOne = document.querySelector('.twenty-one');
+const twentyTwo = document.querySelector('.twenty-two');
+const twentyThree = document.querySelector('.twenty-three');
+const twentyFour = document.querySelector('.twenty-four');
+
+spotArray = [one, two, three, four, five, six, seven, eight, nine, ten, eleven, twelve, thirteen, fourteen, fifteen, sixteen, seventeen, eighteen, nineteen, twenty, twentyOne, twentyTwo, twentyThree, twentyFour]
+
+function modalDisplay() {
+
+  for (let i = 0; i < spotArray.length; i++) {
+
+    const spot = spotArray[i];
+    const taken = spot.getAttribute('data-taken')
+    
+    if (taken === 'taken') {
+      spot
     }
   }
-};
-
-const delButtonHandler = async (event) => {
-  if (event.target.hasAttribute('data-id')) {
-    const id = event.target.getAttribute('data-id');
-
-    const response = await fetch(`/api/projects/${id}`, {
-      method: 'DELETE',
-    });
-
-    if (response.ok) {
-      document.location.replace('/profile');
-    } else {
-      alert('Failed to delete project');
-    }
-  }
-};
+}
 
 
 // custom-handle (slider for time change ui.value)
@@ -52,11 +99,8 @@ $( function() {
   });
 } );
 
-document
-  .querySelector('.new-project-form')
-  .addEventListener('submit', newFormHandler);
+spotOne.addEventListener('click', modalDisplay);
 
-document
-  .querySelector('.project-list')
-  .addEventListener('click', delButtonHandler);
-
+function updateTextInput(val) {
+  document.getElementById('textInput').value=val; 
+}
