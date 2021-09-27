07eab0f2ae3179d86ca6979060f93ca4277c63ae
lyndseyjw <lyndseyjwatson@gmail.com>
2021-09-26 18:35:28 -0700

    trying to update

--- controllers/api/neighborhood-routes.js
+++ controllers/api/neighborhood-routes.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 const { ParkingSpot, User } = require('../../models');
-const withAuth = require('../utils/auth');
+const withAuth = require('../../utils/auth');
 
 // The `/api/ParkingSpot` endpoint
 
